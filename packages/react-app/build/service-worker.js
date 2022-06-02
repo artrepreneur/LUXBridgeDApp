3382ee8690843f8d344b7a8c51ba1612ef441464
artrepreneur <46770824+artrepreneur@users.noreply.github.com>
2022-06-02 11:32:49 -0400

    Random Updates

--- packages/react-app/build/service-worker.js
+++ packages/react-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0a54fafa7a8317e0b5850dae3a31791f.js"
+  "./precache-manifest.17ea30e842453c627ceb005ce2cf4031.js"
 );
 
 self.addEventListener('message', (event) => {
